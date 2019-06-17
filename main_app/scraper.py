b17abc5a2e37eee63bfb275fdcb7c865e4dba30b
Forrest Smith <forrestsmith@Forrests-MacBook-Pro.local>
2019-06-17 15:02:34 -0700

    prep for pull on model changes

--- main_app/scraper.py
+++ main_app/scraper.py
@@ -2,47 +2,177 @@ from requests_html import HTML, HTMLSession
 import csv
 
 
-# csv_file = open('winery_scrape.csv', 'w')
-# csv_writer = csv.writer(csv_file)
-# csv_writer.writerow(['name', 'address', 'desc', 'price'])
+csv_file = open('winery_scrape.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['name', 'address', 'desc', 'price'])
 
 session = HTMLSession()
-s = session.get('https://www.sonoma.com/businesses/?category=Wineries')
-wineries = s.html.find('.article-secondary')
+a = session.get('https://www.sonoma.com/businesses?category=Wineries')
+wineries = a.html.find('.article-secondary')
 for winery in wineries:
-   name = winery.find('.article-body-meta h4')
-   address = winery.find('.article-body-meta>h6')
-   desc = winery.find('.article-body-entry>p')
-   price = winery.find('.bizdetail__price>span')
-   print(name[0].text)
-   print(address[0].text)
-   print(desc[0].text)
-   if len(price) > 0: 
-      print(price[0].text)
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
    else:
-      print('No price data available')
-   # print(price)
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
+
+b = session.get('https://www.sonoma.com/businesses?category=Wineries&page=2')
+wineries = b.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
+
+
+c = session.get('https://www.sonoma.com/businesses?category=Wineries&page=3')
+wineries = c.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
+
+
+d = session.get('https://www.sonoma.com/dusinesses?category=Wineries&page=4')
+wineries = d.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
 
 
 
-# tastings = s.html.find('.article-body-actions li:has(h3)')
+e = session.get('https://www.sonoma.com/businesses?category=Wineries&page=5')
+wineries = e.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
 
 
-# for name in names:
-#    print(name.text)
+f = session.get('https://www.sonoma.com/businesses?category=Wineries&page=6')
+wineries = f.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
 
-# for address in addresses:
-#    print(address.text)
 
-# for des in desc:
-#    print(des.text)
 
-# for tasting in tastings:
-#    print(tasting.text)
+g = session.get('https://www.sonoma.com/businesses?category=Wineries&page=7')
+wineries = g.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
 
-# for price in prices:
-#    if price:
-#       # print(f'From {price.text}')
-#       print(price)
-#    else:
-#       print('No Price data available')
+
+h = session.get('https://www.sonoma.com/businesses?category=Wineries&page=8')
+wineries = h.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
+
+
+i = session.get('https://www.sonoma.com/businesses?category=Wineries&page=9')
+wineries = i.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
+
+
+j = session.get('https://www.sonoma.com/businesses?category=Wineries&page=10')
+wineries = j.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
+
+
+k = session.get('https://www.sonoma.com/businesses?category=Wineries&page=11')
+wineries = k.html.find('.article-secondary')
+for winery in wineries:
+   name = winery.find('.article-body-meta h4')[0].text
+   address = winery.find('.article-body-meta>h6')[0].text
+   desc = winery.find('.article-body-entry>p')[0].text
+   price_find = winery.find('.bizdetail__price>span')
+   price = None
+   if len(price_find):
+      price = f'From {price_find[0].text}'
+   else:
+      price = 'No price data available'
+   csv_writer.writerow([name, address, desc, price])
+
+
+
+csv_file.close() 
+
